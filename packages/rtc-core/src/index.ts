b3a5acd6de504c4948e7cb7123c1163d13e5105c
LJK <jolri24@gmail.com>
2026-07-26 23:28:14 +0900

    feat(rtc): prepare media before joining a room

--- packages/rtc-core/src/index.ts
+++ packages/rtc-core/src/index.ts
@@ -1,4 +1,17 @@
 export {
+  PrejoinMedia,
+  createPrejoinMedia,
+  type PrejoinLocalMediaSnapshot,
+  type PrejoinMediaDevice,
+  type PrejoinMediaIssue,
+  type PrejoinMediaIssueCode,
+  type PrejoinMediaListener,
+  type PrejoinMediaOptions,
+  type PrejoinMediaSnapshot,
+  type PrejoinMediaStatus,
+} from './prejoin-media.js';
+
+export {
   RoomSession,
   createRoomSession,
   type ChatMessage,
@@ -8,6 +21,7 @@ export {
   type RoomIssue,
   type RoomSessionListener,
   type RoomSessionOptions,
+  type RoomSessionRecoveryOptions,
   type RoomSessionSnapshot,
   type RoomSessionStatus,
 } from './room-session.js';
