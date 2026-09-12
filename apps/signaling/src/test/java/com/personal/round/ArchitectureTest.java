45f7c0039a4607d52ccdacec024b0a0a0b64e215
LJK <jolri24@gmail.com>
2026-09-12 10:09:10 +0900

    검사: 진입 계층 의존 규칙 강화

--- apps/signaling/src/test/java/com/personal/round/ArchitectureTest.java
+++ apps/signaling/src/test/java/com/personal/round/ArchitectureTest.java
@@ -1,5 +1,9 @@
 package com.personal.round;
 
+import static com.tngtech.archunit.core.domain.JavaClass.Predicates.resideInAnyPackage;
+import static com.tngtech.archunit.core.domain.JavaClass.Predicates.resideOutsideOfPackage;
+import static com.tngtech.archunit.core.domain.JavaClass.Predicates.simpleNameEndingWith;
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
 
 import com.tngtech.archunit.base.DescribedPredicate;
@@ -23,6 +27,18 @@ class ArchitectureTest {
 							!candidate.isInterface()
 									&& (candidate.isAnnotatedWith(Repository.class)
 											|| candidate.isMetaAnnotatedWith(Repository.class)));
+
+	private static final DescribedPredicate<JavaClass> REPOSITORY_TYPES =
+			simpleNameEndingWith("Repository")
+					.or(CONCRETE_SPRING_REPOSITORIES)
+					.as("Repository 인터페이스 또는 구현체");
+
+	private static final DescribedPredicate<JavaClass> WEBSOCKET_HANDLER_DEPENDENCIES =
+			resideOutsideOfPackage("com.personal.round..")
+					.or(resideInAnyPackage(
+							"com.personal.round.protocol..",
+							"com.personal.round.signaling.."))
+					.as("외부 라이브러리 또는 ROUND 프로토콜과 시그널링 클래스");
 
 	@ArchTest
 	static final ArchRule PROTOCOL_DOES_NOT_DEPEND_ON_OUTER_LAYERS =
@@ -58,8 +74,7 @@ class ArchitectureTest {
 					.that()
 					.areAnnotatedWith(RestController.class)
 					.should()
-					.dependOnClassesThat()
-					.haveSimpleNameEndingWith("Repository")
+					.dependOnClassesThat(REPOSITORY_TYPES)
 					.because("컨트롤러는 저장소 대신 서비스에 의존해야 한다");
 
 	@ArchTest
@@ -73,16 +88,10 @@ class ArchitectureTest {
 
 	@ArchTest
 	static final ArchRule WEBSOCKET_HANDLERS_USE_ONLY_SIGNALING_AND_PROTOCOL_FEATURES =
-			noClasses()
+			classes()
 					.that()
 					.haveSimpleNameEndingWith("WebSocketHandler")
 					.should()
-					.dependOnClassesThat()
-					.resideInAnyPackage(
-							"com.personal.round.auth..",
-							"com.personal.round.config..",
-							"com.personal.round.health..",
-							"com.personal.round.net..",
-							"com.personal.round.turn..")
+					.onlyDependOnClassesThat(WEBSOCKET_HANDLER_DEPENDENCIES)
 					.because("WebSocket 핸들러는 프로토콜 해석과 시그널링 서비스 호출만 담당해야 한다");
 }
